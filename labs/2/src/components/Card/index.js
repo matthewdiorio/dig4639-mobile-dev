39b0677c096e25e961f6c7c3051e47be0de2363b
matthewdiorio <37198911+matthewdiorio@users.noreply.github.com>
2020-02-08 20:02:33 -0500

    started lab 2

---
+++ labs/2/src/components/Card/index.js
@@ -0,0 +1,21 @@
+import "./index.css";
+
+class Card {
+    constructor(props) {
+        // Stores the argument in this.props
+        this.props = props;
+        // Creates a new HTML DIV element
+        this.element = document.createElement("div");
+        let span = document.createElement("span");
+        this.element.appendChild(span);
+        span.innerHTML = this.props.content;
+        this.element.className = "card";
+    }
+
+
+    render() {
+        return this.element;
+    }
+}
+
+export default Card;
