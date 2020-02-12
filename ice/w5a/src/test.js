fe8983c6b1b84e52d089d6f553a962077488c2cf
Matthew Diorio <ma131076@ucf.edu>
2020-02-12 12:20:58 -0500

    completed #95

--- ice/w5a/src/test.js
+++ ice/w5a/src/test.js
@@ -1,29 +1,29 @@
 class User {
 
-    constructor(props){
-        this.props = props;
+    constructor (props){
+        this.props = props
     }
     printName(){
       //  console.log(this.userName);
-        console.log(`${this.props.userName}, id is ${this.props.id}`);
+        console.log(`${this.props.userName}, id is ${this.props.id}`)
     }
 }
 
-var myUser = new User({userName:"Jessica",id:5});
-var myUser2 = new User({userName:"Amber",id:3});
-var myUser3 = new User({userName:"JLucasessica",id:2});
+var myUser = new User({userName:"Jessica",id:5})
+var myUser2 = new User({userName:"Amber",id:3})
+var myUser3 = new User({userName:"JLucasessica",id:2})
 
-myUser.printName();
-myUser2.printName();
-myUser3.printName();
+myUser.printName()
+myUser2.printName()
+myUser3.printName()
 
 function myFunction(){
-    return this;
+    return this
 }
 
 var objLiteral = {
     a: 5,
     b: 6,
     c: myFunction
-};
+}
 
