7260c5759e447388d57c97dab7995c5882167437
Matthew Diorio <ma131076@ucf.edu>
2020-02-24 12:17:09 -0500

    #98

---
+++ demo/w8a/todo-app/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+const todoList = [
+  {
+    content: 'Task 1', priority: 2, completed: true
+  },
+  {
+    content: 'Task 2', priority: 1, completed: true
+  },
+  {
+    content: 'Task 3', priority: 3, completed: false
+  }
+
+]
+
+
+
+function TodoItem(props) {
+  return <p>{props.content}</p>
+}
+
+function App() {
+    const todoListFiltered = todoList.filter((value) => value.completed)
+    let todoArray = todoListFiltered.map(
+    (value) =>  <TodoItem content={value.content} />
+  )
+  // filter?
+
+  /*
+  const todoArray = [
+    <TodoItem content="Item 1"/>,
+     <TodoItem content="Item 2"/>,
+     <TodoItem content="Item 3"/> 
+  ]
+  */
+  return (
+
+      todoArray.filter((v) => v.completed).map(
+          (v) => <TodoItem content={v.content} />)
+
+  );
+}
+
+export default App;
